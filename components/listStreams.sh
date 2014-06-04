d7c19fe1f67dffb4eb77a96c245d37a39da66f8e
emmedemme <milani.emanuele.83@gmail.com>
2014-06-04 17:30:17 +0200

    usage instructions added as comments

--- components/listStreams.sh
+++ components/listStreams.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Extracts information about the streams in a media file.
+# The input filename is expected as command line argument.
+# The output file is in json format and is written to the current working
+# directory. Its name is the basename of the input file with a "_streams.json"
+# suffix.
+# Requires the ffprobe executable (part of the ffmpeg project).
+
+
 EXTRACT="/usr/bin/ffprobe"
 PARAMS="-loglevel quiet -show_streams -print_format json $1"
 
--- components/mp42ogg.sh
+++ components/mp42ogg.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-#CONV="/usr/bin/echo"
+# Converts a video file to ogg.
+# The input filename is expected as command line argument.
+# The ogg file is written to the current working directory and has the same
+# basename as the input file.
+# Requires the ffmpeg executable.
+
 CONV="/usr/bin/ffmpeg"
 PARAMS="-loglevel quiet -i ""$1"
 
